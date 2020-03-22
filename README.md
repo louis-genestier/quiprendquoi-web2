2385d338b2ccf5be474c17f0ea0caf96ed0dac16
louis.genestier <louis.genestier@gmail.com>
2020-03-22 17:56:47 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Qui prend quoi
+
+## Installation
+
+`npm install`
+
+`npm run start`
+
+## Améliorations apportées
+
+- Affichage de la liste des items sur la page événément (`app.js`, `party.pug`)
+- Possibilité de supprimer un item (`app.js`, `party.pug`)
+- Rafraîchissement automatique des items avec les WebSockets (`socket.js`, `scripts/sockets.js`)
+- Utilisation de l'api de Notification pour signaler a l'utilisateur l'ajout ou la suppression d'items (`scripts/sockets.js`)
+
+
+## Article personnel
+
+### Sujet : Les WebSockets  
+
+> WebSocket est une technologie évoluée qui permet d'ouvrir un canal de communication bidirectionnelle entre un navigateur (côté client) et un serveur. Avec cette API vous pouvez envoyer des messages à un serveur et recevoir ses réponses de manière événementielle sans avoir à aller consulter le serveur pour obtenir une réponse.
+Source: [MDN](https://developer.mozilla.org/fr/docs/Web/API/WebSockets_API)  
+
+
--- scripts/socket.js
+++ scripts/socket.js
@@ -2,22 +2,6 @@ const party = window.location.pathname;
 const socket = io(party);
 const items = document.querySelector('.items');
 
-socket.on('newItem', (item) => {
-  console.log(item);
-  const div = document.createElement('div');
-  const p = document.createElement('p');
-  const text = document.createTextNode(`${item.user} - ${item.name}`);
-  p.appendChild(text);
-  div.appendChild(p);
-  items.appendChild(div);
-  notify('Un item à été ajouté !');
-});
-
-socket.on('deleteItem', () => {
-  window.location.reload();
-  notify('Un item a été supprimé !');
-});
-
 function notify(msg) {
   if (!("Notification" in window)) return;
 
@@ -33,3 +17,19 @@ function notify(msg) {
     });
   }
 }
+
+socket.on('newItem', (item) => {
+  console.log(item);
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  const text = document.createTextNode(`${item.user} - ${item.name}`);
+  p.appendChild(text);
+  div.appendChild(p);
+  items.appendChild(div);
+  notify('Un item à été ajouté !');
+});
+
+socket.on('deleteItem', () => {
+  window.location.reload();
+  notify('Un item a été supprimé !');
+});
